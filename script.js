2931514a40279a5f132d67c7f0b2670b2d22757c
Mirek <115916534+mireksira1@users.noreply.github.com>
2022-12-05 19:05:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,24 @@ function downloadspeed() {
     var downspeed = document.getElementsByName("speed")[0].value;
     var filesize = document.getElementsByName("size")[0].value;
 
+    var speedType = document.getElementsByName("speed-type")[0].value;
+    var sizeType = document.getElementsByName("size-type")[0].value;
+
+    switch(speedType){
+        case "Kb":
+            downspeed = (downspeed / 8) / 1024;
+            break;
+        case "KB":
+            downspeed = downspeed / 1024;
+            break;
+        case "Mb":
+            downspeed = downspeed / 8;
+            break;
+        case "MB":
+            downspeed = downspeed;
+            break;
+    }
+
     if (filesize != 0 & downspeed != 0) {
         var time = filesize / downspeed;
         time = time.toFixed(2);
